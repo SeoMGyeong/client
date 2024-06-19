f529c2f38b5cf74e9dab44e09570d025623634a4
SeoMGyeong <book3022@gmail.com>
2024-06-19 11:03:48 +0900

    20240618

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
-import { Fragment } from "react/jsx-runtime";
+interface ProductType {
+  id: number;
+  name: string;
+  explanation: string;
+  price: number;
+}
 
 const App = () => {
-  return <h1>여기는 쇼핑몰입니다.</h1>;
+  const products: ProductType[] = [
+    {
+      id: 0,
+      name: "Iphone 15 Max",
+      explanation: "아이폰 설명을 적어줍니다.",
+      price: 1230000,
+    },
+    {
+      id: 1,
+      name: "Ipad Pro",
+      explanation: "아이패드 설명을 적어줍니다.",
+      price: 1500000,
+    },
+  ]; // 서버구동시 필요없음.
+  console.log(products);
+  return (
+    <>
+      {products.map((product) => (
+        <div>
+          <div>{product.id}</div>
+          <div>{product.name}</div>
+          <div>{product.explanation}</div>
+          <div>{product.price}</div>
+        </div>
+      ))}
+      <h1>{products[0].id}</h1>
+      <h1>{products[0].name}</h1>
+      <h1>{products[0].explanation}</h1>
+      <h1>{products[0].price}</h1>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Greeting.tsx
@@ -0,0 +1,8 @@
+const Greeting = () => {
+    const userName = "이순신";
+    return(
+        <h1>당신의 이름은 {userName} 입니다.</h1>
+    );
+};
+
+export default Greeting;
